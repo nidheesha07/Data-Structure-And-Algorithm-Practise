504441418647ab971bfbc2d81929689136c470aa
nidheesha07 <nidheesha2007@gmail.com>
2026-06-03 16:45:16 +0530

    update

---
+++ Dsa_in_python/arithmetic.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the number: "))
+b=int(input("Enter the other number: "))
+sum=a+b
+sub=a-b
+print("Sum of 2 numbers=",sum)
+print("Subtraction of 2 numbers=",sub)
--- Dsa_in_python/sum.py
+++ Dsa_in_python/sum.py
@@ -2,4 +2,4 @@ a=(int(input("Enter the number: ")))
 b=int(input("Enter the other number: "))
 sum=a+b
 print("Sum of 2 numbers=",sum)
-#print result
+#print result+
